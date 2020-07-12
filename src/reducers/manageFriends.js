15fd56f7e6afc48f182b0797c903f37f6acc7b00
github username <github email address>
2020-07-12 13:39:56 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,25 @@
-export function manageFriends(state, action){
+export function manageFriends(state={friends: []}, action){
+    // console.log("state", state, "action", action)
+
+    switch (action.type) {
+        case 'ADD_FRIEND':
+            console.log("attempting to ADD FRIEND")
+            let newState = {...state}
+            newState.friends.push(action.friend)
+            return newState
+        
+        case 'REMOVE_FRIEND':
+            console.log("attempting to REMOVE FRIEND")
+            console.log("state", state, "action", action)
+            console.log(action.id)
+            let newStateOfFriends =  {...state}
+            newStateOfFriends.friends = newStateOfFriends.friends.filter (friend =>
+                friend.id !== action.id)
+            // newStateOfFriends = newStateOfFriends.filter( friend =>
+            //     friend.id !== action.id)
+            return newStateOfFriends
+
+        default:
+            return state
+    }
 }
